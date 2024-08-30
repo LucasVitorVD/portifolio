8cd8f785ca77befd564351a977f314a65e13bfb0
LucasVitorVD <lucaskirito85@gmail.com>
2024-08-30 15:54:06 -0300

    fix: fixing some styles & adding proper assets

--- src/components/sections/hero/Hero.tsx
+++ src/components/sections/hero/Hero.tsx
@@ -1,4 +1,8 @@
 import { Button } from "@/components/ui/button";
+import { Linkedin, Github } from "lucide-react";
+import ProfilePhoto from "../../../../public/photos/IMG-20240127-WA0082.jpg"
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Hero() {
   return (
@@ -8,13 +12,12 @@ export default function Hero() {
           <p className="text-md md:text-2xl">Desenvolvedor Web</p>
           <div className="text-[2.75rem] leading-tight font-title lg:leading-[9rem] md:text-8xl lg:text-[9rem]">
             <h1>Desenvolvedor</h1>
-            <p className="md:hollow-text lg:z-50 lg:relative lg:left-32">Web Full-Stack</p>
+            <p className="md:hollow-text lg:z-50 lg:relative lg:left-32">
+              Web Full-Stack
+            </p>
           </div>
           <p className="text-secondary mt-6 md:w-[600px] text-wrap">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore sequi
-            modi enim obcaecati sit et temporibus deserunt officiis quibusdam
-            inventore? Placeat reprehenderit temporibus quas saepe id voluptatibus
-            earum vel incidunt.
+            Sou um desenvolvedor <strong>full-stack</strong>, dedicado a aprender e crescer na área da tecnologia, com foco em criar soluções inovadoras que proporcionem experiências incríveis aos usuários!
           </p>
           <div className="flex flex-wrap items-center gap-8 mt-12">
             <Button asChild>
@@ -28,15 +31,40 @@ export default function Hero() {
             </a>
           </div>
         </div>
-        <figure className="space-y-8">
-          <img
-            src="https://images.pexels.com/photos/27945902/pexels-photo-27945902/free-photo-of-bruxa-vermelha.jpeg?auto=compress&cs=tinysrgb&w=1280&h=650&dpr=1"
-            alt="profile image"
-            className="grayscale border border-black"
-          />
-          <figcaption className="text-secondary text-center uppercase font-bold italic">
-            Prazer em te conhecer! <br />
-            Como posso ajudar?
+        <figure className="flex flex-col items-center gap-8">
+          <div className="flex gap-2">
+            <Image
+              src={ProfilePhoto}
+              alt="profile image"
+              width={450}
+              height={400}
+              className="grayscale border border-black"
+            />
+            <div className="hidden md:flex flex-col gap-4 justify-end">
+              <Link
+                href="https://www.linkedin.com/in/lucasvvicente/"
+                target="_blank"
+                rel="external"
+                className="bg-secondary text-primary p-2 rounded-full"
+              >
+                <Linkedin />
+              </Link>
+
+              <Link
+                href="https://github.com/LucasVitorVD"
+                target="_blank"
+                rel="external"
+                className="bg-secondary text-primary p-2 rounded-full"
+              >
+                <Github />
+              </Link>
+            </div>
+          </div>
+          <figcaption>
+            <p className="text-secondary uppercase font-bold italic">
+              Prazer em te conhecer! <br />
+              Como posso ajudar?
+            </p>
           </figcaption>
         </figure>
       </div>
